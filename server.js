b4526da99e3f1578fcab709fe558eafde62346a8
ip-hr <igor.zg3@gmail.com>
2019-08-20 18:21:48 +0200

    server

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
-app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
+
 const port = process.env.PORT || 3000;
-app.listen(port);
+
+const app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+app.listen(port, function () {
+    console.log('App started at port ', this.address().port);
+});
